1a6d8253d7042f778ac92ab1f097b73872037492
Zaldy <worranter@gmail.com>
2024-01-25 13:59:15 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello, World!\n');
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
